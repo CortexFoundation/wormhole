9b08fe6881e65d4cdbafc19ee84aef9b56f1ca9c
ucwong <ucwong@126.com>
2025-07-25 07:51:42 +0800

    tracker health check opt

--- config.go
+++ config.go
@@ -25,13 +25,16 @@ const (
 )
 
 var (
-	BestTrackerUrl = []string{"https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt",
+	BestTrackerUrl = []string{
+		"https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt",
 		"https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_best.txt",
 		"https://ngosang.github.io/trackerslist/trackers_best.txt",
 		// ip addr trackers
 		"https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt",
 		"https://ngosang.github.io/trackerslist/trackers_best_ip.txt",
 		"https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_best_ip.txt",
+
+		"https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt",
 	}
 
 	ColaUrl = []string{"https://github.com/CortexFoundation/cola/releases/download/1.0.0/cola.txt"}
--- wormhole.go
+++ wormhole.go
@@ -17,8 +17,15 @@
 package wormhole
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"net/url"
 	"strings"
 	"time"
@@ -55,8 +62,11 @@ func (wh *Wormhole) Tunnel(hash string) error {
 func (wh *Wormhole) BestTrackers() (ret []string) {
 	defer wh.cl.SetTimeout(time.Second * 10)
 
+	var hc, uc int
+
 	for _, ur := range BestTrackerUrl {
 		log.Debug("Fetch trackers", "url", ur)
+		fmt.Println(ur)
 		resp, err := wh.cl.R().Get(ur)
 
 		if err != nil || resp == nil || len(resp.String()) == 0 {
@@ -79,8 +89,13 @@ func (wh *Wormhole) BestTrackers() (ret []string) {
 			//	wg.Add(1)
 			go func(ss string) {
 				//		defer wg.Done()
-				if err := wh.healthCheck(ss); err == nil {
+				if t, err := wh.healthCheck(ss); err == nil {
 					//ret = append(ret, s)
+					if t == 0 {
+						hc++
+					} else if t == 1 {
+						uc++
+					}
 					retCh <- ss
 				} else {
 					//retCh <- ""
@@ -112,7 +127,9 @@ func (wh *Wormhole) BestTrackers() (ret []string) {
 			//}
 		}
 
-		ret = make([]string, 0, len(str))
+		if ret == nil {
+			ret = make([]string, 0, len(str))
+		}
 		for i := 0; i < len(str); i++ {
 			select {
 			case x := <-retCh:
@@ -128,8 +145,10 @@ func (wh *Wormhole) BestTrackers() (ret []string) {
 		}
 
 		//wg.Wait()
+		fmt.Println(hc)
+		fmt.Println(uc)
 
-		if len(ret) > 0 {
+		if len(ret) > CAP {
 			return
 		}
 		wh.cl.SetTimeout(time.Second * 10)
@@ -138,39 +157,40 @@ func (wh *Wormhole) BestTrackers() (ret []string) {
 	return
 }
 
-func (wh *Wormhole) healthCheck(s string) error {
+func (wh *Wormhole) healthCheck(s string) (int, error) {
 	log.Debug("Global best trackers", "url", s)
 	switch {
 	case strings.HasPrefix(s, "http"), strings.HasPrefix(s, "https"):
-		if _, err := wh.cl.R().Post(s); err != nil {
+		//if _, err := wh.cl.R().Post(s); err != nil {
+		if err := checkHTTPTracker(s); err != nil {
 			log.Warn("tracker failed", "err", err)
 			// TODO
-			return err
+			return 0, err
 		} else {
 			//ret = append(ret, s)
-			return nil
+			return 0, nil
 		}
 	case strings.HasPrefix(s, "udp"):
 		if u, err := url.Parse(s); err == nil {
 			if host, port, err := net.SplitHostPort(u.Host); err == nil {
 				if err := ping(host, port); err == nil {
 					//ret = append(ret, s)
-					return nil
+					return 1, nil
 				} else {
 					log.Warn("UDP ping err", "s", s, "err", err)
 					// TODO
-					return err
+					return 1, err
 				}
 			}
 		} else {
-			return err
+			return 1, err
 		}
 	default:
 		log.Warn("Other protocols trackers", "s", s)
-		return errors.New("invalid url protocol")
+		return -1, errors.New("invalid url protocol")
 	}
 
-	return errors.New("unhealthy tracker url")
+	return -1, errors.New("unhealthy tracker url")
 }
 
 func (wh *Wormhole) ColaList() mapset.Set[string] {
@@ -193,11 +213,157 @@ func (wh *Wormhole) ColaList() mapset.Set[string] {
 	return m
 }
 
-func ping(host string, port string) error {
-	address := net.JoinHostPort(host, port)
-	conn, err := net.DialTimeout("udp", address, 1*time.Second)
-	if conn != nil {
-		defer conn.Close()
+func random20Bytes() []byte {
+	b := make([]byte, 20)
+	rand.Read(b)
+	return b
+}
+
+func buildAnnounceURL(base string) (string, error) {
+	parsed, err := url.Parse(base)
+	if err != nil {
+		return "", err
 	}
-	return err
+
+	infoHash := random20Bytes()
+	peerID := []byte("-GT0001-" + hex.EncodeToString(random20Bytes())[:12])
+	values := url.Values{
+		"info_hash":  {string(infoHash)},
+		"peer_id":    {string(peerID)},
+		"port":       {"6881"},
+		"uploaded":   {"0"},
+		"downloaded": {"0"},
+		"left":       {"0"},
+		"compact":    {"1"},
+		"event":      {"started"},
+	}
+
+	parsed.RawQuery = values.Encode()
+	return parsed.String(), nil
+}
+
+func checkHTTPTracker(base string) error {
+	fullURL, err := buildAnnounceURL(base)
+	if err != nil {
+		return fmt.Errorf("invalid tracker URL: %w", err)
+	}
+
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Get(fullURL)
+	if err != nil {
+		return fmt.Errorf("HTTP request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected HTTP status: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read tracker response: %w", err)
+	}
+
+	if bytes.Contains(body, []byte("failure reason")) {
+		return fmt.Errorf("tracker responded with failure")
+	}
+
+	fmt.Printf("Tracker responded (%d bytes)\n", len(body))
+	return nil
+}
+
+func ping(host, port string) error {
+	address := net.JoinHostPort(host, port)
+
+	return checkUDPTracker(address)
+
+	/*raddr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		return fmt.Errorf("failed to resolve address: %w", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, raddr)
+	if err != nil {
+		return fmt.Errorf("failed to dial UDP: %w", err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte{})
+	if err != nil {
+		return fmt.Errorf("failed to send UDP packet: %w", err)
+	}
+
+	return nil*/
+}
+
+/*
+	func ping(host string, port string) error {
+		address := net.JoinHostPort(host, port)
+		raddr, err1 := net.ResolveUDPAddr("udp", address)
+		if err1 != nil {
+			return err1
+		}
+		conn, err := net.DialUDP("udp", nil, raddr)
+		if conn != nil {
+			defer conn.Close()
+		}
+		return err
+	}
+*/
+const (
+	udpTimeout     = 5 * time.Second
+	actionConnect  = 0
+	actionAnnounce = 1
+	protocolID     = 0x41727101980
+)
+
+func checkUDPTracker(trackerURL string) error {
+	host, port, err := net.SplitHostPort(trackerURL[6:])
+	if err != nil {
+		return fmt.Errorf("invalid tracker URL: %w", err)
+	}
+
+	addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(host, port))
+	if err != nil {
+		return fmt.Errorf("resolve failed: %w", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return fmt.Errorf("dial failed: %w", err)
+	}
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(udpTimeout))
+
+	transactionID := make([]byte, 4)
+	rand.Read(transactionID)
+
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.BigEndian, uint64(protocolID))    // protocol ID
+	binary.Write(&buf, binary.BigEndian, uint32(actionConnect)) // action = connect
+	buf.Write(transactionID)                                    // transaction ID
+
+	_, err = conn.Write(buf.Bytes())
+	if err != nil {
+		return fmt.Errorf("send connect request failed: %w", err)
+	}
+
+	resp := make([]byte, 16)
+	_, err = conn.Read(resp)
+	if err != nil {
+		return fmt.Errorf("connect response failed: %w", err)
+	}
+
+	if len(resp) < 16 || resp[0] != 0 || !bytes.Equal(resp[4:8], transactionID) {
+		return fmt.Errorf("invalid connect response")
+	}
+
+	connectionID := resp[8:16]
+
+	fmt.Println("Tracker responded to connect request.")
+	fmt.Printf("Connection ID: %x\n", connectionID)
+	return nil
 }
